184102396efe7d87cde898eb9dac00895a5f1d82
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2016-02-27 17:41:58 -0800

    Creating more generic execution.

--- CompareAll.py
+++ CompareAll.py
@@ -4,18 +4,86 @@ from BruteForceMatching import get_brute_force_mapping
 
 from BipartiteMatching import get_bipartite_matching
 
-from Clustering import get_cluster_dict
+import Clustering
+
+import ClusteringBestRandomConsensus
+
+import ClusteringBestRandomClusterMerge
+
+import pickle
 
 
-def get_brute_force_with_clustering_mapping(sequence_kmer_list, design_kmer_list):
+class MappingMethod:
 
-    cluster_dict = get_cluster_dict(sequence_kmer_list, design_kmer_list)
+    BRUTE_FORCE = 0
+
+    BIPARTITE = 1
+
+
+class ClusteringFeatures:
+
+    CLUSTER_SIZE_HARD_STOP = 0
+
+    RANDOM_BEST_CONSENSUS = 1
+
+    RANDOM_BEST_CLUSTER = 2
+
+
+def get_mapping(sequence_kmer_list,
+                design_kmer_list,
+                mapping_paramenter,
+                clustering_parameter_list):
+
+    use_cluster_size_hard_stop = False
+    if ClusteringFeatures.CLUSTER_SIZE_HARD_STOP in clustering_parameter_list:
+        use_cluster_size_hard_stop = True
+
+    use_random_best_consensus = False
+    if ClusteringFeatures.RANDOM_BEST_CONSENSUS in clustering_parameter_list:
+        use_random_best_consensus = True
+
+    use_random_best_cluster_merge = False
+    if ClusteringFeatures.RANDOM_BEST_CLUSTER in clustering_parameter_list:
+        use_random_best_cluster_merge = True
+
+    if not use_random_best_consensus and not use_random_best_cluster_merge:
+        cluster_dict = Clustering.get_cluster_dict(sequence_kmer_list, design_kmer_list, use_cluster_size_hard_stop)
+
+    if use_random_best_consensus and not use_random_best_cluster_merge:
+        cluster_dict = ClusteringBestRandomConsensus.get_cluster_dict(sequence_kmer_list, design_kmer_list, use_cluster_size_hard_stop)
+
+    if not use_random_best_consensus and use_random_best_cluster_merge:
+        cluster_dict = ClusteringBestRandomClusterMerge.get_cluster_dict(sequence_kmer_list, design_kmer_list, use_cluster_size_hard_stop)
 
     consensus_kmer_list = cluster_dict.keys()
 
-    brute_force_mapping_list = get_brute_force_mapping(consensus_kmer_list, design_kmer_list)
+    if mapping_paramenter == MappingMethod.BRUTE_FORCE:
 
-    return brute_force_mapping_list
+        consensus_mapping_list = get_brute_force_mapping(consensus_kmer_list, design_kmer_list)
+
+    else:
+
+        consensus_mapping_list = get_bipartite_matching(consensus_kmer_list, design_kmer_list)
+
+    sequence_kmer_mapping_list = []
+
+    for consensus_mapping in consensus_mapping_list:
+
+        consensus_kmer = consensus_mapping[0]
+
+        consensus_mapping_score = consensus_mapping[1]
+
+        design_kmer = consensus_mapping[2]
+
+        sequence_kmer_list = cluster_dict[consensus_kmer]
+
+        for sequence_kmer in sequence_kmer_list:
+
+            sequence_kmer_mapping_list.append((sequence_kmer, consensus_mapping_score, design_kmer))
+
+    # pickle.dump(sequence_kmer_mapping_list, open("sequence_kmer_mapping_list.p", "wb"))
+
+    return sequence_kmer_mapping_list
 
 
 def main():
@@ -24,17 +92,13 @@ def main():
 
     design_kmer_list = kmers.get_design_kmers()
 
-    brute_force_mapping_list = get_brute_force_mapping(sequence_kmer_list, design_kmer_list)
+    # brute_force_mapping_list = get_brute_force_mapping(sequence_kmer_list, design_kmer_list)
+    #
+    # bipartite_matching_list = get_bipartite_matching(sequence_kmer_list, design_kmer_list)
 
-    bipartite_matching_list = get_bipartite_matching(sequence_kmer_list, design_kmer_list)
-
-    clustering_matching_list = get_brute_force_with_clustering_mapping(sequence_kmer_list, design_kmer_list)
-
-    print(len(brute_force_mapping_list))
-
-    print(len(bipartite_matching_list))
-
-    print(len(clustering_matching_list))
+    # print(len(brute_force_mapping_list))
+    #
+    # print(len(bipartite_matching_list))
 
 
 if __name__ == "__main__":
